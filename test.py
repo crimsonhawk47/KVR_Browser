08737f623d48b01afed4b1336fad3bab64fb1260
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-06-23 03:15:09 -0500

    Deleted test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import webbrowser, os, RegisterChrome
-BHandle = RegisterChrome.Get('chrome')
-site = 'https://www.kvraudio.com/forum/'
-print(os.environ['MAC_CHROME_PATH'])
-BHandle.open(site)
-webbrowser.open(site)

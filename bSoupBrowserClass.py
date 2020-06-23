834fca9beb47b3660d5792ae8eb86eb399a0f7f6
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-06-23 14:38:15 -0500

    Added counter to get eles

--- bSoupBrowserClass.py
+++ bSoupBrowserClass.py
@@ -2,7 +2,10 @@ import requests, bs4
 class BSoupBrowser:
     def __init__(self):
         self.searchedeles = []
+        self.count = 0
     def GetEles(self, sitelink, initialHtmlPath, *extraHtmlPaths):
+        print(f'Get Eles has run {self.count} times')
+        self.count+= 1
         res = requests.get(sitelink, headers={ #Fakes a user
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"})
         res.raise_for_status()

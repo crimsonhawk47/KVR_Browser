5575a1f67a18ef9c219f4e6be9958cd3440e6131
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-06-24 21:26:42 -0500

    Extracted getResponse from GetEles

--- bSoupBrowserClass.py
+++ bSoupBrowserClass.py
@@ -3,15 +3,19 @@ class BSoupBrowser:
     def __init__(self):
         self.lastSearchedEles = []
         self.count = 0
-    def GetEles(self, sitelink, initialHtmlPath, *extraHtmlPaths):
+    def GetResponse(self, siteLink):
+        response = requests.get(siteLink, headers={ #Fakes a user
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"})
+        response.raise_for_status()
+        return response
+        
+    def GetEles(self, siteLink, initialCssSelector, *extraCssSelectors):
         print(f'Get Eles has run {self.count} times')
         self.count+= 1
-        res = requests.get(sitelink, headers={ #Fakes a user
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"})
-        res.raise_for_status()
+        res = self.GetResponse(siteLink)
         oSoup = bs4.BeautifulSoup(res.text, features = 'lxml')
-        listOfElements = oSoup.select(initialHtmlPath)
-        for htmlPath in extraHtmlPaths:
+        listOfElements = oSoup.select(initialCssSelector)
+        for htmlPath in extraCssSelectors:
             listOfElements += oSoup.select(htmlPath)
         self.lastSearchedEles = listOfElements
         return listOfElements

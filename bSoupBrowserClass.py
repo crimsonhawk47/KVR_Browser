8d3141f6fdec7cd81d6791062094f7e11ac102fb
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-06-24 23:49:58 -0500

    Get Eles can now work off the last response

--- bSoupBrowserClass.py
+++ bSoupBrowserClass.py
@@ -2,17 +2,23 @@ import requests, bs4
 class BSoupBrowser:
     def __init__(self):
         self.lastSearchedEles = []
-        self.__getElesRunCount = 0
-    def __GetResponse(self, siteLink):
-        response = requests.get(siteLink, headers={ #Fakes a user
+        self.response = None
+        # self.__getElesRunCount = 0
+    def GetResponse(self, site):
+        response = requests.get(site, headers={ #Fakes a user
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"})
         response.raise_for_status()
+        self.response = response
         return response
-        
-    def GetEles(self, siteLink, initialCssSelector, *extraCssSelectors):
-        print(f'Get Eles has run {self.count} times')
-        self.getElesRunCount+= 1
-        res = self.__GetResponse(siteLink)
+
+    def GetEles(self,  initialCssSelector, *extraCssSelectors, site=False):
+        # print(f'Get Eles has run {self.__getElesRunCount} times')
+        # self.__getElesRunCount+= 1
+        res = None
+        if site:
+            res = self.GetResponse(site)
+        else:
+            res = self.response
         oSoup = bs4.BeautifulSoup(res.text, features = 'lxml')
         listOfElements = oSoup.select(initialCssSelector)
         for htmlPath in extraCssSelectors:

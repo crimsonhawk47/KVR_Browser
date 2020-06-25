6c81af996e54733b50bf9f2db49d824b65c81589
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-06-24 22:29:56 -0500

    Made getResponse a private method

--- bSoupBrowserClass.py
+++ bSoupBrowserClass.py
@@ -2,8 +2,8 @@ import requests, bs4
 class BSoupBrowser:
     def __init__(self):
         self.lastSearchedEles = []
-        self.count = 0
-    def GetResponse(self, siteLink):
+        self.getElesRunCount = 0
+    def __GetResponse(self, siteLink):
         response = requests.get(siteLink, headers={ #Fakes a user
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"})
         response.raise_for_status()
@@ -11,8 +11,8 @@ class BSoupBrowser:
         
     def GetEles(self, siteLink, initialCssSelector, *extraCssSelectors):
         print(f'Get Eles has run {self.count} times')
-        self.count+= 1
-        res = self.GetResponse(siteLink)
+        self.getElesRunCount+= 1
+        res = self.__GetResponse(siteLink)
         oSoup = bs4.BeautifulSoup(res.text, features = 'lxml')
         listOfElements = oSoup.select(initialCssSelector)
         for htmlPath in extraCssSelectors:

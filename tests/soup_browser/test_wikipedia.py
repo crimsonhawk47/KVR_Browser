6e536d131d2bad35e98d1f1265ff1c499d95a859
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-07-01 14:22:05 -0500

    Added bSoupBrowser fixtures to tests

--- tests/soup_browser/test_wikipedia.py
+++ tests/soup_browser/test_wikipedia.py
@@ -1,19 +1,25 @@
 from src.bSoupBrowserClass import BSoupBrowser
 import pytest
-browser = BSoupBrowser()
-site = "https://en.wikipedia.org/wiki/Python_(programming_language)"
-browser.GetResponse(site)
-browser.MakeSoup()
+    
+@pytest.fixture(scope='module')
+def browser():
+    browser = BSoupBrowser()
+    site = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+    browser.GetResponse(site)
+    browser.MakeSoup()
+    print('--------------setup----------------')
+    return browser
 
 @pytest.mark.rest
-def test_wikipedia_title():
+def test_wikipedia_title(browser):
     titleElement = browser.SelectByCss("#firstHeading")[0]
     title = titleElement.text
     assert "Python" in title
 
-@pytest.mark.rest
-def test_python_website_is_listed():
-    infoboxElement = browser.SelectByCss('.infobox a')
-    pythonWebsite = next(url.text for url in infoboxElement if "python.org" in url.text)
-    assert pythonWebsite == "www.python.org"
 
+@pytest.mark.rest
+def test_python_website_is_listed(browser):
+    infoboxElement = browser.SelectByCss('.infobox a')
+    pythonWebsite = next(
+        url.text for url in infoboxElement if "python.org" in url.text)
+    assert pythonWebsite == "www.python.org"
--- tests/soup_browser/test_wikipedia_static.py
+++ tests/soup_browser/test_wikipedia_static.py
@@ -2,12 +2,16 @@ from src.bSoupBrowserClass import BSoupBrowser
 from bs4 import BeautifulSoup
 import pytest
 
+@pytest.fixture
+def browser():
+    with open('./tests/soup_browser/pythonWikipedia.html', encoding="utf-8") as file:
+            browser = BSoupBrowser()
+            browser.MakeSoup(file=file)
+    return browser
+
 
 @pytest.mark.static
-def test_wikipedia_static_title():
-    with open('./tests/soup_browser/pythonWikipedia.html', encoding="utf-8") as file:
-        browser = BSoupBrowser()
-        browser.MakeSoup(file=file)
+def test_wikipedia_static_title(browser):
         assert isinstance(browser.soup, BeautifulSoup)
         titleElement = browser.SelectByCss('#firstHeading')[0]
         title = titleElement.text

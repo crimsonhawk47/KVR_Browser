d4f667b37cccea538b1a1edb4efea7337766d5b2
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-07-01 12:42:29 -0500

    Added exception test

--- tests/soup_browser/test_wikipedia_static.py
+++ tests/soup_browser/test_wikipedia_static.py
@@ -1,14 +1,16 @@
 from src.bSoupBrowserClass import BSoupBrowser
 import pytest
 browser = BSoupBrowser()
-site = "https://en.wikipedia.org/wiki/Python_(programming_language)"
-browser.GetResponse(site)
-browser.MakeSoup()
+file = open('./tests/soup_browser/pythonWikipedia.html', encoding="utf-8")
 
 @pytest.mark.static
 def test_wikipedia_static_title():
-    file = open('./tests/soup_browser/pythonWikipedia.html', encoding="utf-8")
-    browser.MakeSoup(file)
+    browser.MakeSoup(file = file)
     titleElement = browser.SelectByCss('#firstHeading')[0]
     title = titleElement.text
     assert "Python" in title
+
+def test_using_makesoup_before_getting_response():
+    browser = BSoupBrowser()
+    with pytest.raises(RuntimeError):
+        assert browser.MakeSoup()
